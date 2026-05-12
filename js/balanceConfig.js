ac8c34f31493070f1f6b4c7f53e8fc93dd58218c
yahhou <yahhou323@gmail.com>
2026-05-12 15:25:59 +0900

    a

--- js/balanceConfig.js
+++ js/balanceConfig.js
@@ -9,9 +9,9 @@ export const PLAYER_BALANCE = {
 
 export const ENEMY_BALANCE = {
   fallbackPlayerLevelRatio: 0.75,
-  hpScalePerLevel: 0.25,
-  statScalePerLevel: 0.15,
-  expScalePerLevel: 0.1,
+  hpScalePerLevel: 0.5,
+  statScalePerLevel: 0.5,
+  expScalePerLevel: 0.25,
   bossLevelBonus: 1,
   bossHpMultiplier: 1.5,
   bossAtkMultiplier: 1.2,
